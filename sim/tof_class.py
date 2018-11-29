d5f2be2b587b02db437ffd231d36dbe130fef8ab
iuri frosio <ifrosio@nvidia.com>
2018-11-29 15:38:50 -0800

    Update tof_class.py

--- sim/tof_class.py
+++ sim/tof_class.py
@@ -1,3 +1,7 @@
+# license:  Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
+#           Licensed under the CC BY-NC-SA 4.0 license
+#           (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode). 
+
 # this file describes lots of different camera classes
 import numpy as np
 import pdb
