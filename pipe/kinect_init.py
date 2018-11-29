63ddc8b5cd1beeed79b1321b7e9e9dc487fbf5d0
iuri frosio <ifrosio@nvidia.com>
2018-11-29 15:32:50 -0800

    Update kinect_init.py

--- pipe/kinect_init.py
+++ pipe/kinect_init.py
@@ -1,3 +1,7 @@
+# license:  Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
+#           Licensed under the CC BY-NC-SA 4.0 license
+#           (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode). 
+
 import numpy as np
 import pickle
 
